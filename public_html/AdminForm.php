c76282e04615c38dedb8ba9f9ab0e8677e006f7e
Sofia Schwartz <sofia.b.schwartz@gmail.com>
2018-02-01 11:12:29 -0800

    Sofia's test

---
+++ public_html/AdminForm.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<frameset cols="10%, *, 10%">
+		<frame>Hello</frame>
+		<frame> 
+			<p style="margin-left: 10px"> Please confirm that these are the open hours for this semester. </p>
+			<iframe src="index.php" height="565px" width="815px" frameborder="0"> </iframe>
+			<button> Continue </button>
+		</frame>
+		<frame>Good bye</frame>
+	</frameset>
+</body>
+</html>
