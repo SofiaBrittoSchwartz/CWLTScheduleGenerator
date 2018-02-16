045a896229c028528b3e9b2068c388cac754beea
Sam Berling <sberling@pugetsound.edu>
2018-02-15 17:55:28 -0800

    Created initial  data structures for Shifts and Tutors; created Master java class that oversees the algorith; created Excel sheet for sample data from Fall 2017

---
+++ Algorithm/src/Tutor.java
@@ -0,0 +1,6 @@
+
+public class Tutor {
+	private String firstName;
+	private String lastName;
+	protected String id; //unique identifying name for this tutor. Will likely be their email address prefix 
+}
